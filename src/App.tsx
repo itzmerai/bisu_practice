8ef9b71db17927f6116943b819258ea916ade50c
itzmerai <159508958+itzmerai@users.noreply.github.com>
2024-08-12 21:26:35 +0800

    change text

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,9 +6,10 @@ function App() {
 
   return (
 <div>
-  <p>gwapo ko</p>
+  <p>gwapo ko ha</p>
 </div>
   )
 }
 
 export default App
+
